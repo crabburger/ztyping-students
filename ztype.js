d8c076e66e54de279b1099a3be6c10d6796e965e
konsomepanchi <93917173+konsomepanchi@users.noreply.github.com>
2021-12-01 20:18:04 +0900

    Update ztype.js

--- ztype.js
+++ ztype.js
@@ -5706,18 +5706,18 @@ ig.module('game.main').requires('impact.game', 'impact.font', 'game.menus.about'
             spawn: [],
             spawnWait: 0.7,
             healthBoost: 0,
-            speedIncrease: 1.05,
+            speedIncrease: 1.01,
             types: [{
                 type: EntityEnemyOppressor,
-                count: 0,
+                count: 1,
                 incEvery: 7
             }, {
                 type: EntityEnemyDestroyer,
-                count: 0,
+                count: 1,
                 incEvery: 3
             }, {
                 type: EntityEnemyMine,
-                count: 9,
+                count: 15,
                 incEvery: 1
             }]
         }
